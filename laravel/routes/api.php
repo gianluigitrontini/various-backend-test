436f5352644e9a308d43d6b9a560d577dafdc509
Gianluigi Trontini <g.trontini@nbsgroup.it>
2023-07-12 17:37:28 +0200

    Implementa api per Customers e Invoices (tutorial Laravel) + Faker

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -18,6 +18,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+  Route::apiResource('customers', CustomerController::class);
+  Route::apiResource('invoices', InvoiceController::class);
+});
+
 Route::get('/hello', function () {
   return "Hello World!";
 });
